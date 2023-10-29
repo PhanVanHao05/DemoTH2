ed31ff16fa0d41917d67ad12cacc1f41096f655f
PhanVanHao05 <21510100909hao@ou.edu.vn>
2023-10-29 20:40:08 +0700

    thay doi den tu feature 2

--- thongbao.cpp
+++ thongbao.cpp
@@ -1,6 +1,38 @@
 include<iostream>
 using namespace std;
-int main()
-{
-cout << "Hello World cua feature_2 nhe nguoi anh em";
+bool isPrime(int number) {
+    if (number < 2) {
+        return false;
+    }
+
+    for (int i = 2; i <= number / 2; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void findPrimes(int start, int end) {
+    cout << "Cac so nguyen to trong khoang tu " << start << " den " << end << " la:\n";
+
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+}
+int main() {
+    int start, end;
+
+    cout << "Nhap vao khoang bat dau: ";
+    cin >> start;
+
+    cout << "Nhap vao khoang ket thuc: ";
+    cin >> end;
+
+    findPrimes(start, end);
+
+    return 0;
 }
